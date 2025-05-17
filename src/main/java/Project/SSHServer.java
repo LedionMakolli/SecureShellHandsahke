51980b9d09dd111189d45d992aff8ad017d191d1
Kimete Sadiku <kimetesadiku8@gmail.com>
2025-05-17 18:30:43 +0200

    identifikimi i serverit SSH dhe shkembimi i celesave DH

--- src/main/java/Project/SSHServer.java
+++ src/main/java/Project/SSHServer.java
@@ -51,3 +51,45 @@ public class SSHServer {
         }
     }
 }
+
+
+
+
+
+ private void sendIdentification() throws IOException {
+            Map<String, Object> identification = new HashMap<>();
+            identification.put("server_id", "SSH_Server_1.0");
+
+            Map<String, List<String>> algorithms = new HashMap<>();
+            algorithms.put("kex", Arrays.asList("diffie-hellman-group14-sha256"));
+            algorithms.put("hostkey", Arrays.asList("ssh-rsa"));
+            algorithms.put("encryption", Arrays.asList("aes256-ctr"));
+            algorithms.put("mac", Arrays.asList("hmac-sha256"));
+
+            identification.put("supported_algorithms", algorithms);
+
+            out.writeObject(identification);
+            out.flush();
+        }
+
+        private byte[] performKeyExchange() throws Exception {
+            // Generate DH key pair
+            KeyPairGenerator dhKpairGen = KeyPairGenerator.getInstance("DH");
+            dhKpairGen.initialize(dhParams);
+            dhKeyPair = dhKpairGen.generateKeyPair();
+
+            // Send server's DH public key
+            out.writeObject(dhKeyPair.getPublic());
+            out.flush();
+
+            // Receive client's DH public key
+            PublicKey clientDhPublicKey = (PublicKey) in.readObject();
+
+            // Perform key agreement
+            keyAgreement = KeyAgreement.getInstance("DH");
+            keyAgreement.init(dhKeyPair.getPrivate());
+            keyAgreement.doPhase(clientDhPublicKey, true);
+
+            byte[] sharedSecret = keyAgreement.generateSecret();
+            return sharedSecret;
+        }
